3cf3d8eeae5fbacf905e161ae170ae9560ed3e15
Tom Nealon <tgnealon@gmail.com>
2013-01-06 16:51:11 +0000

    Initial commit.

---
+++ src/clustering/KMeans2d.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clustering;
+
+/**
+ *
+ * @author tom
+ */
+public class KMeans2d {
+    private DataReader dataReader;
+    private int clusterNo;
+    
+    public KMeans2d(DataReader dataReader, int clusterNo) {
+        this.dataReader = dataReader;
+        this.clusterNo = clusterNo;
+    }
+    
+}
